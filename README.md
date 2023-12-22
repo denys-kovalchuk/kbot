c782e36fecfae49564616c428cfc963a709003c6
denys-kovalchuk <kovalchuk.denys1@gmail.com>
2023-12-22 22:20:24 +0200

    initial push

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple telegram bot on GO lang
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "github.com/denys-kovalchuk/kbot/cmd"
+
+func main() {
+	cmd.Execute()
+}
