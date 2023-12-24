000225a8d998e9618e00378e80b8424f77865af7
denys-kovalchuk <kovalchuk.denys1@gmail.com>
2023-12-24 18:42:58 +0200

    upd

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/den-vasyliev/kbot/cmd"
+import "github.com/denys-kovalchuk/kbot/cmd"
 
 func main() {
 	cmd.Execute()
