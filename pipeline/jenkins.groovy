9b3a36d861d6d9657b25357934b055a9961c8965
denys-kovalchuk <118395901+denys-kovalchuk@users.noreply.github.com>
2024-01-02 10:14:42 +0200

    Update jenkins.groovy

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,17 +1,60 @@
 pipeline {
+pipeline {
     agent any
+
     parameters {
-
-        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
-
+        choice(name: 'OS', choices: ['linux', 'macos', 'windows'], description: 'Pick OS')
+        choice(name: 'ARCH', choices: ['amd64','arm', 'arm64'], description: 'Pick ARCH')
     }
+
+    environment {
+	GITHUB_TOKEN=credentials('github-token')
+	REPO = 'https://github.com/denys-kovalchuk/kbot'
+	REGISTRY = 'ghcr.io/denys-kovalchuk'
+	BRANCH = 'main'
+	TARGETOS = "${params.OS}"
+	TARGETARCH = "${params.ARCH}"
+    }
+
     stages {
-        stage('Example') {
+
+        stage('clone') {
             steps {
-                echo "Build for platform ${params.OS}"
+                echo 'Clone Repository'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
 
-                echo "Build for arch: ${params.ARCH}"
+        stage('test') {
+            steps {
+                echo 'Testing started'
+                sh "make test"
+            }
+        }
 
+        stage('build') {
+            steps {
+                echo "Building binary for platform ${params.OS} on ${params.ARCH} started"
+                sh "make ${params.OS} ${params.ARCH}"
+            }
+        }
+
+        stage('image') {
+            steps {
+                echo "Building image for platform ${params.OS} on ${params.ARCH} started"
+                sh "make image-${params.OS} ${params.ARCH}"
+            }
+        } 
+
+	stage('login to GHCR') {
+            steps {
+                sh "docker login ghcr.io -u denys-kovalchuk -p $GHCR_TOKEN"
+            }
+        }
+
+	stage('push image') {
+            steps {
+                sh "make push"
             }
         }
     }
